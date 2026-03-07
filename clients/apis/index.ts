6dda73f5d9a23d43ea23b80e87dcc7ad6a5595b1
NomenaFitahiana <nomenafitahiana.randrianirina@gmail.com>
2026-03-07 03:59:49 +0300

    chore: generate new client & docs

--- clients/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./AuthApi";
-export * from "./GoalApi";
-export * from "./LabelApi";
-export * from "./ProjectApi";
-export * from "./TransactionApi";
-export * from "./WalletApi";
